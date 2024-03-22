2c0e6866d012e978ff8267bab9b5d0cfe56fa222
vinisvp <viniciuspedro0905@gmail.com>
2024-03-22 15:14:51 -0300

    Added the reactive form, with a table

--- src/app/aluno/aluno.component.ts
+++ src/app/aluno/aluno.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Student } from '../student';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-aluno',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrl: './aluno.component.css'
 })
 export class AlunoComponent {
+  students: Student[] = [];
+  studentFormGroup: FormGroup;
 
+  constructor(private formBuilder: FormBuilder){
+    this.studentFormGroup = formBuilder.group({
+      id:[''],
+      name:[''],
+      course:['']
+    });
+  }
+
+  save(){
+    this.students.push(this.studentFormGroup.value);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AlunoComponent } from './aluno/aluno.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { AlunoComponent } from './aluno/aluno.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

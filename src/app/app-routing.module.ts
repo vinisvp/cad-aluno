4770b16a808e66c782d26727891b9f2bc1354084
vinisvp <viniciuspedro0905@gmail.com>
2024-04-19 15:00:05 -0300

    Added a navbar and footer, made the routing of the components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CoursesComponent } from './courses/courses.component';
+import { StudentsComponent } from './students/students.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'courses', component: CoursesComponent},
+  {path: 'students', component: StudentsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
